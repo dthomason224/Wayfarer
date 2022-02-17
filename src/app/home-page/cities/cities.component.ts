8d853c417e42dd63869f19bc3f4f235cc0389c52
Dalton <dthomason24@gmail.com>
2022-02-17 15:49:21 -0500

    changed cities component to use json server api

--- src/app/home-page/cities/cities.component.ts
+++ src/app/home-page/cities/cities.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-cities',
@@ -7,14 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CitiesComponent implements OnInit {
 
-  cities: string[] = [];
+  cities: Observable<any> = this.http.get("/api/cities");
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.cities.push("San Francisco");
-    this.cities.push("London");
-    this.cities.push("Gibraltar");
+
   }
 
 }
